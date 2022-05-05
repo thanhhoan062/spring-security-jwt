a172c3fbce3f767484522650801a9cce5850c298
thanhhoan062 <nguyenthanhhoan2605@gmail.com>
2022-05-05 19:55:51 +0700

    #commit

---
+++ demo/src/main/java/com/hoannt/security/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.hoannt.security.repository;
+
+import com.hoannt.security.models.ERole;
+import com.hoannt.security.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
