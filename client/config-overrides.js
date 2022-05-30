d9ad659b6b6618fae14da28b518e37418c5095b8
thanhhoan062 <nguyenthanhhoan2605@gmail.com>
2022-05-30 23:12:48 +0700

    #commit start client

---
+++ client/config-overrides.js
@@ -0,0 +1,36 @@
+
+const {
+    override,
+    fixBabelImports,
+    addLessLoader,
+} = require("customize-cra");
+
+module.exports = override(
+    fixBabelImports("import", {
+        libraryName: "antd", libraryDirectory: "es", style: true // change importing css to less
+    }),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: {
+            "@layout-body-background": "#FFFFFF",
+            "@layout-header-background": "#FFFFFF",
+            "@layout-footer-background": "#FFFFFF"
+        }
+    })
+);
+
+// const rewireLess = require('react-app-rewire-less');
+
+// module.exports = function override(config, env) {
+//     config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
+
+//     config = rewireLess.withLoaderOptions({
+//         modifyVars: {
+//             "@layout-body-background": "#FFFFFF",
+//             "@layout-header-background": "#FFFFFF",
+//             "@layout-footer-background": "#FFFFFF"
+//         },
+//         javascriptEnabled: true
+//     })(config, env);
+//     return config;
+// };
